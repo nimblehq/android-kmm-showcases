f56ac5282fdd5f5c42f164ffd0357d430855d4c8
Edgars Simanovskis <edgars@nimblehq.co>
2022-03-22 02:23:40 +0700

    [ng-4] Created Job details view UI for ios

---
+++ KMM_Showcase/KMM_Showcase/Extensions/Color+hex.swift
@@ -0,0 +1,24 @@
+//
+//  Color+hex.swift
+//  KMM_Showcase
+//
+//  Created by Edgars Simanovskis on 22/3/2565 BE.
+//  Copyright © 2565 BE orgName. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+    
+    static let blackColor = Color(hex: 0x171923)
+    static let greenColor = Color(hex: 0x2BC65D)
+}
