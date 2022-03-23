08be1bf75f35d94639d883db31384211c74e4484
hoangnguyen92dn <hoang.l@nimblehq.co>
2022-03-24 00:57:59 +0700

    [#5] Show the response to UI on Android

--- KMM_Showcase/src/main/java/co/nimblehq/kmm/showcase/android/JobDetailsFragment.kt
+++ KMM_Showcase/src/main/java/co/nimblehq/kmm/showcase/android/JobDetailsFragment.kt
@@ -2,10 +2,18 @@ package co.nimblehq.kmm.showcase.android
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import co.nimblehq.kmm.showcase.Api
 import co.nimblehq.kmm.showcase.android.base.BaseFragment
 import co.nimblehq.kmm.showcase.android.databinding.FragmentJobDetailsBinding
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 class JobDetailsFragment : BaseFragment<FragmentJobDetailsBinding>() {
+
+    private val mainScope = MainScope()
+    private val api = Api()
 
     override fun getViewBinding(
         layoutInflater: LayoutInflater,
@@ -18,5 +26,22 @@ class JobDetailsFragment : BaseFragment<FragmentJobDetailsBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar(R.string.job_details_title)
+
+        mainScope.launch {
+            kotlin.runCatching {
+                api.todoTask()
+            }.onSuccess {
+                binding.titleTv.text = it
+            }.onFailure {
+                binding.titleTv.text = it.localizedMessage
+            }
+
+            Toast.makeText(requireContext(), "hello2", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mainScope.cancel()
     }
 }
