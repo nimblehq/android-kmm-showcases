703ad38d3aa514cbd9d20f5056c350a21ec9b745
hoangnguyen92dn <hoang.l@nimblehq.co>
2022-03-25 16:07:16 +0700

    [#15] Add Loading progress bar to show while fetching Job details

--- androidApp/src/main/java/co/nimblehq/kmm/showcase/android/JobDetailsFragment.kt
+++ androidApp/src/main/java/co/nimblehq/kmm/showcase/android/JobDetailsFragment.kt
@@ -25,12 +25,12 @@ class JobDetailsFragment : BaseFragment<FragmentJobDetailsBinding>() {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar(R.string.job_details_title)
 
-        Toast.makeText(requireContext(), "Loading", Toast.LENGTH_SHORT).show()
+        binding.pbJobDetailsLoading.visibility = View.VISIBLE
+        binding.clJobDetailsContainer.visibility = View.GONE
         mainScope.launch {
             kotlin.runCatching {
                 getJobDetailUseCase.execute()
             }.onSuccess {
-                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
                 displayJobDetails(it)
             }.onFailure {
                 Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_SHORT).show()
@@ -40,18 +40,28 @@ class JobDetailsFragment : BaseFragment<FragmentJobDetailsBinding>() {
 
     private fun displayJobDetails(jobDetailUiModel: JobDetailUiModel) {
         with(binding) {
+            pbJobDetailsLoading.visibility = View.GONE
+            clJobDetailsContainer.visibility = View.VISIBLE
             with(jobDetailUiModel) {
                 listOf(
                     tvJobDetailsPriceByAreaValue,
                     tvJobDetailsBottomPanelPriceValue
                 ).forEach {
-                    it.text = getString(R.string.job_details_price_by_area_value, minimumPrice, maximumPrice)
+                    it.text = getString(
+                        R.string.job_details_price_by_area_value,
+                        minimumPrice,
+                        maximumPrice
+                    )
                 }
 
                 lJobDetailsDateTime.run {
                     tvJobDetailsDate.text = confirmedAt.formatDate()
                     tvJobDetailsTime.text =
-                        getString(R.string.job_details_date_time_time_value, timeSlotStart, timeSlotEnd)
+                        getString(
+                            R.string.job_details_date_time_time_value,
+                            timeSlotStart,
+                            timeSlotEnd
+                        )
                 }
 
                 lJobDetailsPlotInfo.run {
